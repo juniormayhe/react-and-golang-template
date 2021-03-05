7fa94dd389cda1c3e6329a4221780c932cd15b0e
Junior Mayhe <juniormayhe@gmail.com>
2021-03-05 20:27:49 +0000

    readme

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,5 @@
 package main
 
-/*
-gopls requires a module at the root of your workspace.
-You can work with multiple modules by opening each one as a workspace folder.
-Improvements to this workflow will be coming soon, and you can learn more here:
-https://github.com/golang/tools/blob/master/gopls/doc/workspace.md.g
-*/
-
 import (
 	"fmt"
 )
---
+++ readme.md
@@ -0,0 +1,5 @@
+# wm-site
+
+## Tutorial
+
+https://tutorialedge.net/projects/chat-system-in-go-and-react/part-1-initial-setup/
