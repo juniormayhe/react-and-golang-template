fc5be0072ddeff61d5aa52bfa3b6e67c63cb396a
Junior Mayhe <juniormayhe@gmail.com>
2021-03-05 22:04:57 +0000

    fixes

--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+	"fmt"
+	"html"
 	"log"
 	"net/http"
+	"path"
 )
 
-func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println(w, "Simple Server")
+func setupRoutes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/send-email", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("send email " + html.EscapeString(r.URL.Path))
 	})
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		buildPath := path.Clean("ui/build")
+		log.Println("buildPath " + buildPath)
+
+		buildURL := fmt.Sprintf("/%s/", buildPath)
+		log.Println("buildURL " + buildURL)
+
+		dir := http.Dir(buildPath)
+		log.Println("dir " + dir)
+
+		stripped := http.FileServer(dir)
+
+		// http.Handle("/", http.FileServer(http.Dir("./")))
+
+		log.Println("root " + html.EscapeString(r.URL.Path))
+		stripped.ServeHTTP(w, r)
+	})
+	return mux
 }
 
 func main() {
-	setupRoutes()
+	handler := setupRoutes()
 
-	http.ListenAndServe(":8080", nil)
+	if err := http.ListenAndServe(":8080", handler); err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import "./app.css";
+import HeaderComponent from "./components/HeaderComponent";
+
+function App() {
+  return (
+    <div className="main">
+      <HeaderComponent />
+    </div>
+  );
+}
+
+export default App;
